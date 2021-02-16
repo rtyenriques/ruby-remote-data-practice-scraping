5d86dafa618aeac04b8d800b6c8d7b49e97a529e
rtyenriques <tooshopboys@gmail.com>
2021-02-16 13:23:19 -0800

    commit tree 25a7db498fe3ff3010cc2cd68563f5948e6bcad5

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# doc = Nokogiri::HTML(open("http://flatironschool.com"/))
+
+doc.css(".site-header__hero")
+
+binding.pry
+
